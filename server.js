96fa315d336ac855f2220be6971a4aff23c6a35d
bailus <bailus@communism9000.(none)>
2011-08-29 17:08:51 +1200

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var io = require('socket.io').listen(1337),
     check = require('validator').check,
     sanitize = require('validator').sanitize,
-    OAuth = require('oauth').OAuth;
 /* production settings for socket.io */
 //io.enable('browser client minification');  // send minified client
 //io.enable('browser client etag');          // apply etag caching logic based on version number
