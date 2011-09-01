d603962bf80ff5dbe835a9a51549131e34f8885e
bailus <bailus@communism9000.(none)>
2011-09-01 23:07:21 +1200

    openid testing

--- server.js
+++ server.js
@@ -639,9 +639,7 @@ var lobby = io.of('/lobby').on('connection',function(socket){  //initial connect
   socket.on('login',function(data){
     var playername = '';
     for (p in players) {
-console.log(data.id);
-console.log(players[p].id);
-      if (data.id == players[p].id) { playername = players[p].nickname; }
+      if (encodeURIComponent(data.id) == players[p].id) { playername = players[p].nickname; }
     }
     if (!(playername == '')) {
 	  socket.set('playername',playername);
