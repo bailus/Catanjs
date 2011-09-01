e228d5df7fd3804dae6506cc33c0407e7df5816c
bailus <bailus@communism9000.(none)>
2011-09-01 13:51:52 +1200

    openid testing

--- server.js
+++ server.js
@@ -710,7 +710,7 @@ connect(
   , connect.cookieParser()
   , connect.session({secret: 'whodunnit'})
   , everyauth.middleware()
-  , connect.router(routes);
+  , connect.router(routes)
 ).listen(80);
 
 
