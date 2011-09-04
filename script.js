3d1ef8fab5aacbfb474616a7b5c613b703575e4e
Sam Bailey <sam.bailus@gmail.com>
2011-09-04 14:42:53 +1200

    lobby css

--- script.js
+++ script.js
@@ -1265,7 +1265,8 @@ function chatsend() {
   return false;
 }
 function lobbychat(playerid,playername,playerservice,data) {
-  $('<div><div class="player"><div class="serviceicon '+playerservice+'"></div>'+playername+'</div>'+data+'</div>').hide().prependTo('#lobbychat').slideDown(100);
+  var date = new Date();
+  $('<div><div class="player"><div class="serviceicon '+playerservice+'"></div>'+playername+'</div><div class="time">'+date.getHours()+':'+date.getMinutes()+'</div>'+data+'</div>').hide().prependTo('#lobbychat').slideDown(100);
 }
 function lobbychatsend() {
   var input = $('#lobbychatinput');
