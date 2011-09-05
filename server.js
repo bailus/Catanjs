4bac1fddeb6f645aadbe77f29bd43b207ee3a8b3
Sam Bailey <sam.bailus@gmail.com>
2011-09-05 21:46:08 +1200

    lobby css

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ addPlayerToDB = function(playerid,playername,func) { //add the player to the dat
   });
 };
 
-addPlayerToDB('321','asdf',function(err){
+addPlayerToDB('321','popopopopopop',function(err){
   if (err) { console.log('error adding player to db'); }
   else {
     getPlayerFromDB('321',function(err,player) {
