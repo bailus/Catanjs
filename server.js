d5d91f571267009bb75b35568de98874598a47e4
Sam Bailey <sam.bailus@gmail.com>
2011-09-06 14:30:52 +1200

    lobby css

--- server.js
+++ server.js
@@ -854,10 +854,10 @@ http.get('/verify', function(req, res){
           } else {
             var player = {'playerid':id,'playername':nickname,'logins':1};
           }
-          /*savePlayer(player,function(err){
+          savePlayer(player,function(err){
             if (err) { res.send('Database Error: '+err); }
             else { res.redirect('http://bailus.no.de/game?id='+id+'&key='+key); }
-          });*/
+          });
         }
       });
     } else {
