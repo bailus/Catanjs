c9b2038e4bf79c38ca80a8d381bc9f9c77f06158
Sam Bailey <sam.bailus@gmail.com>
2011-09-10 19:16:56 +1200

    ...

--- make.sh
+++ make.sh
@@ -11,7 +11,7 @@ echo '' >> index.htm
 echo '<style>' >> index.htm
 cat style.css >> index.htm
 echo '' >> index.htm
-echo '</style><link rel="stylesheet" type="text/css" href="themes/default.css" id="themeCSS"></head><body class="background">' >> index.htm
+echo '</style><link rel="stylesheet" type="text/css" href="http://bailus.no.de.nyud.net/themes/default.css" id="themeCSS"></head><body class="background">' >> index.htm
 cat body.htm >> index.htm
 echo '' >> index.htm
 echo '<script>' >> index.htm
