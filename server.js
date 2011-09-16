97663ec64242b589bf56df507f770e8761b56a85
Sam Bailey <sam.bailus@gmail.com>
2011-09-16 15:43:44 +1200

    ...

--- server.js
+++ server.js
@@ -891,10 +891,10 @@ http.get('/verify', function(req, res){
     }
   });
 });
-http.get('/guest/?(:nickname)?',function(req,res){
+http.get('/guest/:nickname',function(req,res){
   var key = Math.floor(Math.random()*10000000000000000);
   var id = +new Date();
-  var nickname = req.params.nickname ? req.params.nickname : 'Guest';
+  var nickname = req.params.nickname.length ? req.params.nickname : 'Guest';
   players.push({'id':id,'nickname':nickname,'service':'guest','key':key});
   res.redirect('/game?id='+id+'&key='+key);
 });
